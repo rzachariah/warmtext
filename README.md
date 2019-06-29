0b9884cd34e1cbe0c446730f0c80629bf8a7b3a4
Ranjith Zachariah <rzachariah@ezesoft.com>
2019-06-29 18:04:49 -0400

    Flesh out messages API

---
+++ README.md
@@ -0,0 +1,9 @@
+## warmtext
+REST api for sending warm texts
+
+## Run it
+```
+yarn
+yarn start
+```
+Find the swagger api at http://localhost:3000/api/v1
--- api/controllers/messages.js
+++ api/controllers/messages.js
@@ -1,8 +1,21 @@
 'use strict';
 
+const config = require('../../config');
+const client = require('twilio')(config.accountSid, config.authToken);
+const Quote = require('inspirational-quotes');
+
 async function sendMessage(req, res) {
-  console.log('Got request');
-  res.status(200).json("Do something...");
+  const request = req.swagger.params.request.value;
+  console.log('Got request', request);
+
+  client.messages
+  .create({
+     body: Quote.getRandomQuote(),
+     from: config.sendingNumber,
+     to: request.targetNumber
+   })
+  .then(message => console.log(message.sid));
+  res.status(200).json("Message sent!");
 }
 
 module.exports = {
